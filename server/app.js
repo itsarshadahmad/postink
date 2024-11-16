53d84713e3eac11a1631f1bc9040b73b624c5d58
Arshad Ahmad <arshadxdev@gmail.com>
2024-11-16 19:01:40 +0530

    feat: logger added

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import fs from "fs";
+import path from "path";
 
 const app = express();
 
@@ -16,12 +18,19 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-// Error logs
+
+// Morgan middleware for logging
+const accessLogStream = fs.createWriteStream(
+    path.join("./log/access.log"),
+    { flags: "a" }
+);
+
 app.use(
     morgan("combined", {
         skip: function (req, res) {
             return res.statusCode < 400;
         },
+        stream: accessLogStream,
     })
 );
 
--- server/routers/home.routes.js
+++ server/routers/home.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { asyncHandler } from "../services/asyncHandler.js";
+import { ApiError } from "../services/ApiError.js";
 
 const app = Router();
 
 app.route("/").get(
     asyncHandler((req, res) => {
-        // throw new ApiError(404, "Test", new Error());
+        throw new ApiError(404, "Test", new Error());
         return res.send("Hello World!");
     })
 );
